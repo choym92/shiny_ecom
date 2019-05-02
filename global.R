22475e488005bfee3e8aad9fda18959c9768f13a
paul.cho <choym92@gmail.com>
2019-05-02 07:26:44 -0400

    commit #2

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-
+library(plyr)
 library(ggplot2)
 library(data.table)
 library(shiny)
@@ -8,6 +8,7 @@ library(shinydashboard)
 library(dplyr)
 library(shinythemes)
 
+# setwd('C:/Users/Paul Cho/Desktop/Bootcamp/Project/Shiny_e_com')
 geo_df <- fread("./datasets/olist_geolocation_dataset.csv", header=TRUE)
 po_master<- fread("./datasets/po_master.csv", header=TRUE)
 choice <- colnames(geo_df)
@@ -23,13 +24,3 @@ po_master %>%
             backorder_ratio = backorder/orderCount) %>% 
   select(product_category,c_state, seller_id, delivered_week, delivered_year,backorder, backorder_ratio) -> backorder_rate
 
-intro_str =  "Marketing has always been an industry that is heavily data related. Big firms spent millions of dollars every year on analysising their marketing data, in terms of 
-finding insights and make their marketing investment wisely. Because of my marketing background, discovering insights from a marketing dataset 
-always insterest me, so I did this project that is using a Brazilian online retail marketplace's sales data to understand the Brazil's E-commerce from mutiple perspectives. This 
-shiny app has three major sections. With geographic section, you could visualize data from a geographic standpoint and see some correlations. The trends section is the part to show
-sales trends in the particular time range you selected, you could also comparing sales volume between categories. The categories section visualize the data from its different
-categories perspective."
-
-intrdata_str = "The dataset I am using is provided by the largest department store in Brazilian marketplaces, called Olist. It inculdes its over 100k orders from late 2016 to 2018.
-The dataset's schema is showing below in the graphy. The data is divided into multiple datasets. It allows us to view the Brazilian E-commerce from mutiple demisions like order status,
-payments information, geographic location and so on."
--- ui.R
+++ ui.R
@@ -4,39 +4,45 @@ library(shinydashboard)
 shinyUI(dashboardPage(skin = "black",
                       
                       
-  ##### Header 
+                      ##Dashboard Header-------------
   dashboardHeader(title = "E-Commerce Demand Fulfillment Dashboard", titleWidth = 450),
-  ##### Sidebar 
+  ##Dashboard Sidebar----------------
   dashboardSidebar(
-    sidebarUserPanel("Paul Cho", "NYC Data Science Academy"),
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Lead Time", tabName = "LT", icon = icon("shipping-fast")),
       menuItem("Geographic", tabName = "geo", icon = icon("map")),
       menuItem("About", tabName = "about", icon = icon("info"))
       )
     ),
-  #### Body
+  ##Dashboard Body----------------
   dashboardBody(        
     tabItems(
-      tabItem(tabName = "home",
+      tabItem(tabName = "about",
               fluidRow(
-                width = 12,
                 box(background = "navy",id = "home",solidHeader = TRUE,width = 12, 
                     h1("About the Project...", align = 'center'), 
                     br(),
                     h3("Supply Chain Management, especially order management side of it, may not sound much interesting to many people. However, it is very important part of the industry which can be improved greatly and there is a huge potential there.
                         Supply Chain network overall is extremely long and complicated process and saving 3-5% of transportation or inventory stocking cost is a big deal to many companies."), 
-                    h3("From the fall of giant brick-and-mortar stores such as Sears, Maceys JCPenney... etc, e-commerce is a must option for retail companies going forward. As many of you all experinced, one of the biggest differentiators between the Amazon and other online retailers' is the speed of delivery."),
+                    h3("As seen by the fall of giant brick-and-mortar stores such as Sears, Maceys JCPenney..., e-commerce is a must option for retail companies going forward. As many of you all experinced, one of the biggest differentiators between the Amazon and other online retailers' is the speed of delivery."),
                     h3("The motive of this is to visualize the time sensitive e-com data from the company Olist for product run-rate analysis to pinpoint the optimal retail insights by providing sales and retail capacity indexes."),
                     br(),br(),
                     img(src = "scm.png", height = 600, width=795),
                     img(src = "data_schema.png", align = 'center', height = 600, width = 795),
-                    h5("This is a Brazilian e-commerce public dataset of orders made at Olist Store. The dataset has information of 100k orders from 2016 to 2018 made at multiple marketplaces in Brazil. (src: Kaggle)", align='center')
+                    p(em("This is a Brazilian e-commerce public dataset of orders made at Olist Store. The dataset has information of 100k orders from 2016 to 2018 made at multiple marketplaces in Brazil. (src: Kaggle)", align='center', style = "font-si25pt"))
                     ))
-              
-              
-              
               ),
+      tabItem(tabName = "LT",
+              fluidRow(
+                column(2,
+                       box("Sidebar")
+                       ),
+                column(10,
+                box(plotOutput("plot1", height = 250)),
+                box(title = "Controls", sliderInput("slider", "Number of observations:", 1, 100, 50))
+                )
+              )),
       tabItem(tabName = "geo",
               fluidRow(
                 column(
